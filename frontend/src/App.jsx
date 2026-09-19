a2abf6fb6eb3291787bd1ed588f80a6ea252505c
Partho Sarathi Debe <parthosarathideb2014@gmail.com>
2026-09-19 18:35:39 +0530

    Redirect root path to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import CreatePoll from "./pages/CreatePoll";
 import VotePoll from "./pages/VotePoll";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/create" element={<CreatePoll />} />
